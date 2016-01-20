e4c85b3a5225648ba18fca4521d1452c06c0416a
root <born2net@gmail.com>
2016-01-20 15:18:32 -0800

    css updates

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ gulp.task('LiveServer', ['_watchSource'], function () {
     server.use(express.static('./'));
     server.listen(8003);
     browserSync({
+        port: 8080,
         proxy: 'localhost:8003',
         reloadDelay: '3000'
     });
