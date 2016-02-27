1085d4413235c462fc0619932e6c8b85752b93e2
root <born2net@gmail.com>
2016-02-26 22:48:31 -0800

    added example on how to manually create template and bind to a view, using CountDown component

--- src/comps/app2/notes/Notes.js
+++ src/comps/app2/notes/Notes.js
@@ -1,4 +1,3 @@
-"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -20,6 +19,7 @@ var Notes = (function () {
         __metadata('design:paramtypes', [])
     ], Notes);
     return Notes;
-}());
+})();
 exports.Notes = Notes;
-//# sourceMappingURL=Notes.js.map
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBzL2FwcDIvbm90ZXMvbm90ZXMudHMiXSwibmFtZXMiOlsiTm90ZXMiLCJOb3Rlcy5jb25zdHJ1Y3RvciJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7O0FBQUEscUJBQTBDLGVBQWUsQ0FBQyxDQUFBO0FBRTFEO0lBTUlBO0lBQ0FDLENBQUNBO0lBUExEO1FBQUNBLGdCQUFTQSxDQUFDQTtZQUNQQSxRQUFRQSxFQUFFQSxPQUFPQTtZQUNqQkEsUUFBUUEsRUFBRUEsMkJBQTJCQTtTQUN4Q0EsQ0FBQ0E7O2NBS0RBO0lBQURBLFlBQUNBO0FBQURBLENBUkEsQUFRQ0EsSUFBQTtBQUhZLGFBQUssUUFHakIsQ0FBQSIsImZpbGUiOiJjb21wcy9hcHAyL25vdGVzL05vdGVzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHtDb21wb25lbnQsIFZpZXdDb250YWluZXJSZWZ9IGZyb20gXCJhbmd1bGFyMi9jb3JlXCI7XHJcblxyXG5AQ29tcG9uZW50KHtcclxuICAgIHNlbGVjdG9yOiAnTm90ZXMnLFxyXG4gICAgdGVtcGxhdGU6IGA8bmctY29udGVudD48L25nLWNvbnRlbnQ+YFxyXG59KVxyXG5cclxuZXhwb3J0IGNsYXNzIE5vdGVzIHtcclxuICAgIGNvbnN0cnVjdG9yKCkge1xyXG4gICAgfVxyXG59XHJcbiJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
