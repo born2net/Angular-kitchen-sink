4d9c06d25cd06a83badcebd29982f8d59368389d
root <born2net@gmail.com>
2016-02-29 09:38:36 -0800

    added support for @CanActivate to block user accessing routes if not auth

--- src/services/AppInjService.ts
+++ src/services/AppInjService.ts
@@ -1,11 +1,10 @@
 import {Injector} from 'angular2/core';
 let appInjectorRef: Injector;
 /**
- * With the help of appInjService we register the main applicaiton
+ * With the help of appInjService we register the main app
  * injector and keep is via closure in appInjectorRef so we can always
- * grab it and easily get a reference to whatever component instance we
- * wish that is provided through our main App.ts
- *
+ * grab it and easily get a reference to whatever component instance we wish,
+ * that is provided through our main App.ts providers
  */
 export const appInjService = (injector?: Injector): Injector => {
     if (injector)
