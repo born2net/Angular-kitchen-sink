a452b6a08598745df595f95db3dc918d317d8add
root <born2net@gmail.com>
2016-03-15 21:03:03 -0700

    added email validator to forms

---
+++ src/validators/EmailValidator.ts
@@ -0,0 +1,36 @@
+import {provide, Directive, forwardRef} from 'angular2/core';
+import {Control, NG_VALIDATORS} from 'angular2/common';
+
+function validateEmailFactory() {
+    return (c: Control) => {
+        let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+
+        return EMAIL_REGEXP.test(c.value) ? null : {
+            validateEmail: {
+                valid: false
+            }
+        };
+    };
+}
+
+@Directive({
+    selector: '[validateEmail][ngControl],[validateEmail][ngModel],[validateEmail][ngFormControl]',
+    providers: [
+        provide(NG_VALIDATORS, {
+            useExisting: forwardRef(() => EmailValidator),
+            multi: true
+        })
+    ]
+})
+export class EmailValidator {
+
+    validator: Function;
+
+    constructor() {
+        this.validator = validateEmailFactory();
+    }
+
+    validate(c: Control) {
+        return this.validator(c);
+    }
+}
