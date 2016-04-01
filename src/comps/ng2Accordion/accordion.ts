7889915fa5b210a4276f3c8a76fb54b3870fcd63
root <born2net@gmail.com>
2016-04-01 13:22:44 -0700

    added more components samples from angulartypescript.com

---
+++ src/comps/ng2Accordion/accordion.ts
@@ -0,0 +1,82 @@
+import {Component, Input, OnDestroy} from 'angular2/core';
+
+@Component({
+  selector: 'accordion',
+  template:`
+  <ng-content></ng-content>
+          `,
+  host: {
+    'class': 'panel-group'
+  }
+})
+export class Accordion {
+  groups: Array<AccordionGroup> = [];
+  
+  addGroup(group: AccordionGroup): void {
+    this.groups.push(group);
+  }
+  
+  closeOthers(openGroup: AccordionGroup): void {
+    this.groups.forEach((group: AccordionGroup) => {
+      if (group !== openGroup) {
+        group.isOpen = false;
+      }
+    });
+  }
+  
+  removeGroup(group: AccordionGroup): void {
+    const index = this.groups.indexOf(group);
+    if (index !== -1) {
+      this.groups.splice(index, 1);
+    }
+  }
+}
+
+@Component({
+  selector: 'accordion-group',
+  template:`
+                <div class="panel panel-default" [ngClass]="{'panel-open': isOpen}">
+                  <div class="panel-heading" (click)="toggleOpen($event)">
+                    <h4 class="panel-title">
+                      <a href tabindex="0"><span>{{heading}}</span></a>
+                    </h4>
+                  </div>
+                  <div class="panel-collapse" [hidden]="!isOpen">
+                    <div class="panel-body">
+                        <ng-content></ng-content>
+                    </div>
+                  </div>
+                </div>
+          `,
+
+})
+export class AccordionGroup implements OnDestroy {
+  private _isOpen:boolean = false;
+  
+  @Input() heading: string;
+  
+  @Input()
+  set isOpen(value: boolean) {
+    this._isOpen = value;
+    if (value) {
+      this.accordion.closeOthers(this);
+    }
+  }
+  
+  get isOpen() {
+    return this._isOpen;
+  }
+  
+  constructor(private accordion: Accordion) {
+    this.accordion.addGroup(this);
+  }
+  
+  ngOnDestroy() {
+    this.accordion.removeGroup(this);
+  }
+  
+  toggleOpen(event: MouseEvent): void {
+    event.preventDefault();
+    this.isOpen = !this.isOpen;
+  }
+}
