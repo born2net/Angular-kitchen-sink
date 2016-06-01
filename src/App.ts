c1d77890ef7f7041da942cb5e0defba4302a9821
root <born2net@gmail.com>
2016-06-01 12:58:10 -0700

    upd loader of hot-loader

--- src/App.ts
+++ src/App.ts
@@ -7,7 +7,7 @@ import "zone.js/dist/long-stack-trace-zone";
 import "reflect-metadata";
 import {ROUTER_DIRECTIVES, ROUTER_PROVIDERS, Routes} from "@angular/router";
 import {bootstrap} from "@angular/platform-browser-dynamic";
-import {Component, provide, ViewEncapsulation, PLATFORM_PIPES, ComponentRef} from "@angular/core";
+import {Component, provide, ViewEncapsulation, PLATFORM_PIPES, ComponentRef, enableProdMode} from "@angular/core";
 import "jspm_packages/github/twbs/bootstrap@3.3.6";
 import "twbs/bootstrap/dist/css/bootstrap.css!";
 import "./styles/style.css!";
