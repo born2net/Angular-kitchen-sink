b9f2dee9b749fc135bb44ba0c5c2a06193b4a047
root <born2net@gmail.com>
2016-06-01 11:57:06 -0700

    update plugin-typescript

--- jspm.config.js
+++ jspm.config.js
@@ -10,7 +10,7 @@ SystemJS.config({
             "angular-platform-browser-dynamic": "npm:@angular/platform-browser-dynamic@2.0.0-rc.1",
             "clean-css": "npm:clean-css@3.4.12",
             "https": "github:jspm/nodelibs-https@0.2.0-alpha",
-            "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.9",
+            "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.17",
             "reflect-metadata": "npm:reflect-metadata@0.1.2",
             "systemjs-hot-reloader": "github:capaj/systemjs-hot-reloader@0.6.0",
             "tty": "github:jspm/nodelibs-tty@0.2.0-alpha"
@@ -208,7 +208,7 @@ SystemJS.config({
         "stream": "github:jspm/nodelibs-stream@0.2.0-alpha",
         "string_decoder": "github:jspm/nodelibs-string_decoder@0.2.0-alpha",
         "text": "github:systemjs/plugin-text@0.0.4",
-        "ts": "github:frankwallis/plugin-typescript@4.0.9",
+        "ts": "github:frankwallis/plugin-typescript@4.0.17",
         "typescript": "npm:typescript@1.8.7",
         "underscore": "npm:underscore@1.8.3",
         "url": "github:jspm/nodelibs-url@0.2.0-alpha",
@@ -884,7 +884,7 @@ SystemJS.config({
                 "xtend": "npm:xtend@4.0.1"
             }
         },
-        "github:frankwallis/plugin-typescript@4.0.9": {
+        "github:frankwallis/plugin-typescript@4.0.17": {
             "map": {
                 "typescript": "npm:typescript@1.8.7"
             }
