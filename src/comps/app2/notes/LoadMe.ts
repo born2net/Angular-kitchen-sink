0a0c01ef34bbeeb7b9eadcd07f896ca9c0d7183c
root <born2net@gmail.com>
2016-06-15 14:39:01 -0700

    working on more samples

--- src/comps/app2/notes/LoadMe.ts
+++ src/comps/app2/notes/LoadMe.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'load-me',
-    template: `<h3 style="background-color: #00b0e8">I'm a component loaded from a Gist!</h3>`
+    template: `<h3 style="background-color: #00b0e8">
+      I am a component loaded from a Gist!</h3>`
 })
 export class LoadMe {
 }
