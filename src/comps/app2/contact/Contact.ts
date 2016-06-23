397210cb9e41ea3216727a9ce7751e679fa77dd3
root <born2net@gmail.com>
2016-06-22 18:55:58 -0700

    updated to rc.3 new forms notes1

--- src/comps/app2/contact/Contact.ts
+++ src/comps/app2/contact/Contact.ts
@@ -7,6 +7,8 @@ import {CommBroker} from "../../../services/CommBroker";
 @Component({
     selector: 'Contact',
     directives: [REACTIVE_FORM_DIRECTIVES],
+    moduleId: __moduleName,
+    templateUrl: 'Contact.html',
     styles: [`
             .ng-invalid {
               border-left: 5px solid #a94442; /* red */
@@ -17,101 +19,7 @@ import {CommBroker} from "../../../services/CommBroker";
            .form-control {
                 width: 20%;
            }
-       `],
-    template: `
-                <small>I am Contact component</small>
-                <ng-content></ng-content>
-                <div class="container">
-                  <div [hidden]="submitted">
-                    <h1>TO DO: Update form to RC.X</h1>
-                    
-                    <form [formGroup]="registerForm">
-                      <label>Firstname:</label>
-                      <input type="text" formControlName="firstname">
-                    
-                      <label>Lastname:</label>
-                      <input type="text" formControlName="lastname">
-                    
-                     <fieldset formGroupName="address">
-                          <label>Street:</label>
-                          <input type="text" formControlName="street">
-                        
-                          <label>Zip:</label>
-                          <input type="text" formControlName="zip">
-                        
-                          <label>City:</label>
-                          <input type="text" formControlName="city">
-                       </fieldset>
-                      <button type="submit">Submit</button>
-                    </form>
-                 </div>
-                 </div>
-
-                    <!-- special tx to ng2-book via http://blog.ng-book.com/the-ultimate-guide-to-forms-in-angular-2/ -->
-
-                    <!-- Importing FORM_DIRECTIVES automatically binds form to ngForm (which is a ControlGroup) and ngSubmit for us -->
-                    <!-- To create a new ControlGroup and Controls implicitly use: -->
-                    <!--  -->
-                    <!-- ngForm and -->
-                    <!-- ngControl -->
-                    <!-- But to bind to an existing ControlGroup and Controls use: -->
-                    <!--  -->
-                    <!-- ngFormModel and -->
-                    <!-- ngFormControl -->
-
-                    <!--<form (ngSubmit)="onSubmit(contactForm.value)" #contactForm="ngForm">-->
-                      <!--<div class="form-group">-->
-                        <!--<label for="name">Name</label>-->
-                        <!--<input type="text" class="form-control" required [(ngModel)]="model.name" (change)="onChange($event)" ngControl="name" #name="ngForm">-->
-                        <!--<div [hidden]="name.valid" class="alert alert-danger">-->
-                          <!--Name is required  field-->
-                        <!--</div>-->
-                      <!--</div>-->
-                      <!--<div class="form-group">-->
-                        <!--<label for="subject">Subject</label>-->
-                        <!--<input type="text" class="form-control" (change)="onChange($event)" [(ngModel)]="model.subject" ngControl="subject" >-->
-                      <!--</div>-->
-                      <!--<div class="form-group">-->
-                        <!--<label for="contactMethod">How should we contact you?</label>-->
-                        <!--<select class="form-control" required (change)="onChange($event)" [(ngModel)]="model.contactMethod" ngControl="contactMethod" #contactMethod="ngForm" >-->
-                          <!--<option *ngFor="let p of contacts" [value]="p">-->
-                            <!--{{p}}-->
-                          <!--</option>-->
-                        <!--</select>-->
-                        <!--<div [hidden]="contactMethod.valid" class="alert alert-danger">-->
-                          <!--contact method is required-->
-                        <!--</div>-->
-                      <!--</div>-->
-                      <!--<label class="pull-left">-->
-                      <!--</label>-->
-                      <!--<br/>-->
-                      <!--<br/>-->
-                      <!--<button type="submit" class="btn btn-default" [disabled]="!contactForm.form.valid">Submit</button>-->
-                    <!--</form>-->
-                  <!--</div>-->
-                  <!--<div [hidden]="!submitted">-->
-                    <!--<h2>You submitted the following:</h2>-->
-                    <!--<div class="row">-->
-                      <!--<div class="col-xs-3">Name</div>-->
-                      <!--<div class="col-xs-9  pull-left">{{ model.name }}</div>-->
-                    <!--</div>-->
-                    <!--<div class="row">-->
-                      <!--<div class="col-xs-3">Subject</div>-->
-                      <!--<div class="col-xs-9 pull-left">{{ model.subject }}</div>-->
-                    <!--</div>-->
-                    <!--<div class="row">-->
-                      <!--<div class="col-xs-3">Power</div>-->
-                      <!--<div class="col-xs-9 pull-left">{{ model.contactMethod }}</div>-->
-                    <!--</div>-->
-                    <!--<div class="row">-->
-                      <!--<div class="col-xs-3">Gender Male</div>-->
-                      <!--<div class="col-xs-9 pull-left">{{ model.male }}</div>-->
-                    <!--</div>-->
-                    <!--<br>-->
-                    <!--<button class="btn btn-default" (click)="submitted=false">Edit</button>-->
-                  <!--</div>-->
-                <!--</div>-->
-                `
+       `]
 })
 
 /**
@@ -119,17 +27,18 @@ import {CommBroker} from "../../../services/CommBroker";
  Usage is implicit as done purely through the View.
 
  Suggestion: I recommend you use explicit Form creation via
- manual instance creation of Controls (inputs) and ControlGroup (Form) as it's a lot
- more flexible, see Notes1.ts for sample code
+ manual instance creation of Controls
  **/
 export class Contact {
     private contacts = ['Call me', 'Email me', 'Page me (old school)'];
     model = new MailModel(1, 'your name', true, this.contacts[0], 'how can we help you?');
     submitted = false;
-    registerForm: FormGroup;
-    constructor(private commBroker:CommBroker, private formBuilder: FormBuilder){
+    registerForm:FormGroup;
+
+    constructor(private commBroker:CommBroker, private formBuilder:FormBuilder) {
         this.commBroker.getService(Consts.Services().Properties).setPropView('Contact');
     }
+
     onSubmit(event) {
         console.log(event);
         if (event.contactMethod.indexOf('Page') > -1) {
@@ -141,6 +50,7 @@ export class Contact {
         this.submitted = true;
     }
     ;
+
     ngOnInit() {
         this.registerForm = this.formBuilder.group({
             firstname: [],
