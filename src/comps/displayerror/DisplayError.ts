cd77f4bc78897e45c0953b124515d136ceee2ceb
root <born2net@gmail.com>
2016-06-24 10:31:15 -0700

    pass in FromGroup instead of DI in display-error

--- src/comps/displayerror/DisplayError.ts
+++ src/comps/displayerror/DisplayError.ts
@@ -4,12 +4,13 @@ import {REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl} from "@angular/forms";
 
 @Component({
     selector: 'display-error',
-    providers: [REACTIVE_FORM_DIRECTIVES],
     template: `<div *ngIf="isDisplayed()">
                     <ng-content></ng-content>
                </div>`
 })
 export class DisplayError {
+    @Input('formGroup')
+    formGroup:FormGroup;
     @Input('control')
     controlName:string;
     @Input()
@@ -19,13 +20,15 @@ export class DisplayError {
     // we inject the form model
     // constructor(@Inject(forwardRef(() => FormGroup)) private formModel:FormGroup) {
     // constructor(@Host() @Inject(forwardRef(() => FormGroup)) private formModel:FormGroup) {
-    constructor(@Host() private formModel:FormControl) {
-        console.log(formModel);
-    }
+
+    // broken in new forms rc.3 so changed to passing in formGroup instead
+    // constructor(@Host() private formModel:FormGroup) {
+    //     console.log(FormGroup);
+    // }
 
     // we then find the control
     ngOnInit() {
-        this.control = this.formModel.find(this.controlName);
+        this.control = this.formGroup.find(this.controlName);
     }
 
     // the div in the template will only be added if
