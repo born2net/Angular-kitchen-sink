541a879595b3458547f3d079bbad93b7284ad237
root <born2net@gmail.com>
2016-06-25 15:42:58 -0700

    fixed ngBookRepeat

--- src/comps/ngForIf/ngBookRepeat.ts
+++ src/comps/ngForIf/ngBookRepeat.ts
@@ -46,13 +46,10 @@ class MyRepeatIf implements DoCheck {
                 console.log('template', this.template);
                 changes.forEachAddedItem((change) => {
                     let view = this.viewContainer.createEmbeddedView(this.template);
-
                     //pre rc.1
                     //view.setLocal('\$implicit', change.item);
-
                     // post rc.1
                     view.context.$implicit = change.item;
-
                     this.views.set(change.item, view);
                 });
                 changes.forEachRemovedItem((change) => {
