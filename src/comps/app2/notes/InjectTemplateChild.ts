d84a371028aaccbf966beadfd439598fe388b87f
root <born2net@gmail.com>
2016-07-12 10:04:20 -0700

    example of injecting template as string at runtime example added

--- src/comps/app2/notes/InjectTemplateChild.ts
+++ src/comps/app2/notes/InjectTemplateChild.ts
@@ -21,7 +21,7 @@ import {TemplateRef} from "@angular/core";
         "[class.is-open]": "isShowingItems"
     },
     template: `
-		<div (click)="toggleItems()" class="dropdown-root" [ngSwitch]="!! value">
+		<div (click)="toggleItems()" class="dropdown-root" [ngSwitch]="!!value">
 			<div *ngSwitchCase="true" class="dropdown-item-content">
 				<template
 					[ngTemplateOutlet]="itemTemplate"
@@ -37,20 +37,13 @@ import {TemplateRef} from "@angular/core";
 		</div>
 
 		<ul *ngIf="isShowingItems" class="dropdown-items">
-			<li
-				*ngFor="let item of items ; let index = index ;"
-				(click)="selectItem( item )"
-				class="dropdown-item">
-
+			<li	*ngFor="let item of items ; let index = index" (click)="selectItem( item )" class="dropdown-item">
 				<div class="dropdown-item-content">
-
 					<template
 						[ngTemplateOutlet]="itemTemplate"
 						[ngOutletContext]="{ item: item, index: index }">
 					</template>
-
 				</div>
-
 			</li>
 		</ul>
 	`
@@ -100,24 +93,17 @@ export class InjectTemplateChild {
     // NOTE: Since this is a one-way data flow, the selection is being emitted rather
     // than applied directly to the value.
     public selectItem(item: any): void {
-
         this.hideItems();
         this.valueChange.emit(item);
-
     }
-
 
     // I show the dropdown items.
     public showItems(): void {
-
         this.isShowingItems = true;
-
     }
-
 
     // I show or hide the dropdown items depending on their current visibility.
     public toggleItems(): void {
-
         this.isShowingItems
             ? this.hideItems()
             : this.showItems()
