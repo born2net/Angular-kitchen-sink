a3ccf6f62450f053f7513c66c39f651850df8963
root <born2net@gmail.com>
2016-10-05 10:07:43 -0700

    fixed case in starwars and tabs

--- src/comps/popup/Popup.ts
+++ src/comps/popup/Popup.ts
@@ -3,6 +3,8 @@ import {Directive, ElementRef} from '@angular/core'
 /**
  * Example of directive exporting its instance variable to the entire template it's hosted
  * in via the exportAs property
+ * only directives need to exportAs so we can access their internals, see:
+ * https://plnkr.co/edit/IlLtBY7Ic9yKiRIpjukf?p=preview
  */
 @Directive({
     selector: ' [ popup ] ',
