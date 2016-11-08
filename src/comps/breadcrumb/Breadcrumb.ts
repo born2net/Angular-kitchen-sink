61974352f926c92908bfa6ca26285b380a0ecb06
root <born2net@gmail.com>
2016-11-08 05:13:39 -0800

    fixed breadcrumb component bug with while loop

--- src/comps/breadcrumb/Breadcrumb.ts
+++ src/comps/breadcrumb/Breadcrumb.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-import { Router, ActivatedRoute, NavigationEnd, Params, PRIMARY_OUTLET } from "@angular/router";
-import "rxjs/add/operator/filter";
-
 /**
  *  reference: http://brianflove.com/2016/10/23/angular2-breadcrumb-using-router/
  **/
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute, NavigationEnd, Params, PRIMARY_OUTLET } from "@angular/router";
+import "rxjs/add/operator/filter";
 
 interface IBreadcrumb {
     label: string;
@@ -39,50 +38,55 @@ export class BreadcrumbComponent implements OnInit {
 
         //subscribe to the NavigationEnd event
         this.router.events.filter(event => event instanceof NavigationEnd).subscribe(event => {
-            //reset breadcrumbs
-            this.breadcrumbs = [];
 
-            //get the root route
-            let currentRoute: ActivatedRoute = this.activatedRoute.root;
-
-            //set the url to an empty string
-            let url: string = "";
-
-            //iterate from activated route to children
-            while (currentRoute.children.length > 0) {
-                let childrenRoutes: ActivatedRoute[] = currentRoute.children;
-
-                //iterate over each children
-                childrenRoutes.forEach(route => {
-                    //set currentRoute to this route
-                    currentRoute = route;
-
-                    //verify this is the primary route
-                    if (route.outlet !== PRIMARY_OUTLET) {
-                        return;
-                    }
-
-                    //verify the custom data property "breadcrumb" is specified on the route
-                    if (!route.snapshot.data.hasOwnProperty(ROUTE_DATA_BREADCRUMB)) {
-                        return;
-                    }
-
-                    //get the route's URL segment
-                    let routeURL: string = route.snapshot.url.map(segment => segment.path).join("/");
-
-                    //append route URL to URL
-                    url += `/${routeURL}`;
-
-                    //add breadcrumb
-                    let breadcrumb: IBreadcrumb = {
-                        label: route.snapshot.data[ROUTE_DATA_BREADCRUMB],
-                        params: route.snapshot.params,
-                        url: url
-                    };
-                    this.breadcrumbs.push(breadcrumb);
-                });
-            }
+            //set breadcrumbs
+            let root: ActivatedRoute = this.activatedRoute.root;
+            this.breadcrumbs = this.getBreadcrumbs(root);
         });
+    }
+
+    private getBreadcrumbs(route: ActivatedRoute, url: string="", breadcrumbs: IBreadcrumb[]=[]): IBreadcrumb[] {
+        const ROUTE_DATA_BREADCRUMB: string = "breadcrumb";
+
+        //get the child routes
+        let children: ActivatedRoute[] = route.children;
+
+        //return if there are no more children
+        if (children.length === 0) {
+            return breadcrumbs;
+        }
+
+        //iterate over each children
+        for (var child of children) {
+            //verify primary route
+            if (child.outlet !== PRIMARY_OUTLET) {
+                continue;
+            }
+
+            //verify the custom data property "breadcrumb" is specified on the route
+            if (!child.snapshot.data.hasOwnProperty(ROUTE_DATA_BREADCRUMB)) {
+                continue;
+            }
+
+            //get the route's URL segment
+            let routeURL: string = child.snapshot.url.map(segment => segment.path).join("/");
+
+            //append route URL to URL
+            url += `/${routeURL}`;
+
+            //add breadcrumb
+            let breadcrumb: IBreadcrumb = {
+                label: child.snapshot.data[ROUTE_DATA_BREADCRUMB],
+                params: child.snapshot.params,
+                url: url
+            };
+            breadcrumbs.push(breadcrumb);
+
+            break;
+        }
+
+        //recursive
+        return this.getBreadcrumbs(child, url, breadcrumbs);
     }
 
 }
