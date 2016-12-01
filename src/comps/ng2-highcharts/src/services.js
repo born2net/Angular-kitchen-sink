1f126c762531d35b8fcf0cce3bb4b2671332bd91
root <born2net@gmail.com>
2016-11-30 16:26:11 -0800

    remove comps js

--- src/comps/ng2-highcharts/src/services.js
+++
@@ -1,9 +0,0 @@
-/* */ 
-// var lang_1 = require('angular2/src/facade/lang');
-// Import all services
-//import {SampleService} from './services/sample.service';
-// Export all services
-//export {SampleService} from './services/sample.service';
-// Export convenience property
-exports.SERVICES = lang_1.CONST_EXPR([]);
-//# sourceMappingURL=services.js.map
