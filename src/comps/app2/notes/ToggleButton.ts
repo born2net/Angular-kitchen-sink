f91ee3ba93bf7d1a6d07a8d252526c5bdd14ff52
root <born2net@gmail.com>
2016-12-16 06:16:43 -0800

    custom banana in a box [(cdMsg)]

--- src/comps/app2/notes/ToggleButton.ts
+++ src/comps/app2/notes/ToggleButton.ts
@@ -22,6 +22,19 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
 `
 })
 export class ToggleButton {
+
+    // here we created our own ngModel using the Change suffix
+    // you can also bind to model as:
+    // <input [value]="cdMsg" (input)="update($event.target.value)"/>
+    //
+    // update(val : string) {
+    //     this.cdMsg = val;
+    //     this.cdMsgChange.emit(this.cdMsg);
+    // }
+    //
+    // or even without sugar:
+    // <msg-app bindon-cdMsg ="msg"> </msg-app>
+
     @Input() on = true;
     @Output() onChange = new EventEmitter();
 
