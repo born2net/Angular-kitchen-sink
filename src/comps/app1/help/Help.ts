d20ce05ef932e37f39e6b01e10eec8bd20666d46
root <born2net@gmail.com>
2016-12-15 17:43:41 -0800

    added greensock animation

--- src/comps/app1/help/Help.ts
+++ src/comps/app1/help/Help.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild, ElementRef} from "@angular/core";
 import {Contributors} from "./contributors/contributors";
 import {CommBroker} from "../../../services/CommBroker";
 import {Consts} from "../../../Conts";
@@ -6,23 +6,38 @@ import {Consts} from "../../../Conts";
 @Component({
     selector: 'Help',
     providers: [Contributors],
+    styles: [`
+        svg {
+          display: block;
+          max-width: 300px;
+          margin: 0 auto;
+        }
+        .left, .shield {
+          fill:#DD0031;
+        }
+        .right {
+          fill:#C3002F;
+        }
+        .a {
+          fill:#FFFFFF;
+        }
+        `],
     template: ` <small>I am Help component and using
                 <a href="https://github.com/valor-software/ng2-bootstrap" target="_blank">ng2-bootstrap</a></small>
                 <br/>
-                <alert type="info">Welcome to Angular2 Bootstrap starter pack!</alert>
-                <!--<rating [(ngModel)]="rate" [max]="max" [readonly]="isReadonly" [titles]="['one','two','three']" ></rating>-->
+                <alert type="info">Some examples on animation and SVG</alert>
                 <br/>
                   <tabs>
-                    <tab [tabtitle]="'About this app'">
-                      <h4>
-                        This App was created as both a starting point for anyone looking to create a scalable application in Angular 2
-                      as it focuses on key concepts of object oriented / component programming.</h4>
-                      <hr/>
-                      <h4>We also try and touch all the major aspects
-                      of the language to give new comments a sample code base to reference for help (well at least until Angular 2 takes over
-                      the universe | Angular 2 - one framework baby!!!).</h4>
-                      <hr/>
-                      <h5>Want to help? visit me on Github</h5>
+                    <tab [tabtitle]="'Greensock animation'">
+                    <a href="https://greensock.com" target="_blank">Click to learn more about green sock</a>
+                    <svg viewBox="0 0 250 250">
+                      <svg:polygon #left class="left" points="125,30 125,30 125,30 31.9,63.2 46.1,186.3 125,230 125,230 125,230 203.9,186.3 218.1,63.2" />
+                      <svg:polygon #right class="right" points="125,30 125,52.2 125,52.1 125,153.4 125,153.4 125,230 125,230 203.9,186.3 218.1,63.2 125,30" />
+                      <svg:path class="a" d="M125,52.1L66.8,182.6h0h21.7h0l11.7-29.2h49.4l11.7,29.2h0h21.7h0L125,52.1L125,52.1L125,52.1L125,52.1
+                        L125,52.1z M142,135.4H108l17-40.9L142,135.4z"/>
+                    </svg>
+                    <hr/>
+                      <h5>Pure CSS animation still works in Angular</h5>
                         <a id="largeGit" href="https://github.com/born2net/Angular-kitchen-sink" target="_blank" class="fa fa-github"></a>
                     </tab>
                     <tab [tabtitle]="'Contributors'" >
@@ -54,10 +69,30 @@ import {Consts} from "../../../Conts";
                 `
 })
 
-export class Help  {
-    constructor(private commBroker:CommBroker) {
+export class Help {
+    constructor(private commBroker: CommBroker) {
     }
-    ngOnInit(){
+
+    @ViewChild('left') left: ElementRef;
+    @ViewChild('right') right: ElementRef;
+
+    ngOnInit() {
         this.commBroker.getService(Consts.Services().App).appResized();
+        TweenMax.to(this.left.nativeElement, 1, {
+            attr: {
+                points: '125,30 125,30 125,30 31.9,30 31.9,230 125,230 125,230 125,230 203.9,186.3 218.1,63.2'
+            },
+            repeat: -1,
+            yoyo: true,
+            ease: Cubic.easeInOut
+        });
+        TweenMax.to(this.right.nativeElement, 1, {
+            attr: {
+                points: '125,30 125,52.2 125,52.1 125,153.4 125,153.4 125,230 125,230 218.1,230 218.1,30 125,30'
+            },
+            repeat: -1,
+            yoyo: true,
+            ease: Cubic.easeInOut
+        });
     }
 }
