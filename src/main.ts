bbac4eb4f41667f6e3700d5c297d396f0b2e5470
root <born2net@gmail.com>
2016-12-16 11:51:14 -0800

    - added fake backend service

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './polyfills.ts';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
-import { AppModule } from './app/';
+import { AppModule } from './app/'; // we don't have to specify the file name as index.ts directory exists in ./app folder
 
 if (environment.production) {
   enableProdMode();
