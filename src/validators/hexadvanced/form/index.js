118d5d84756cd89745c554e71039130c719931c8
root <born2net@gmail.com>
2016-12-17 07:53:58 -0800

    added advanced example of NG_VALUE_ACCESSOR (hexadecimal  form component)

---
+++ src/validators/hexadvanced/form/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require("./animations"));
+__export(require("./element-base"));
+__export(require("./validate"));
+__export(require("./value-accessor"));
