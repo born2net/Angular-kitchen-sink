4961b114037b4249e613e702556cfe8d3aa7939f
root <born2net@gmail.com>
2016-12-16 18:07:44 -0800

    3 more example of dynamic component creation

--- src/comps/app2/notes/Notes3.ts
+++ src/comps/app2/notes/Notes3.ts
@@ -7,44 +7,40 @@ import {DynaFactoryResService} from "./DynaFactoryResService";
 @Component({
     selector: 'Notes3',
     providers: [DynaFactoryResService],
-    styles: [
-        `
+    styles: [`
         .colorBlue span {
             color: blue;
-        }`
-    ],
+        }
+    `],
     templateUrl: './Notes3.html'
 })
 
 export class Notes3 extends NotesBase {
-
-
-
-    constructor(protected sliderPanel:Sliderpanel, protected commBroker:CommBroker) {
+    constructor(protected sliderPanel: Sliderpanel, protected commBroker: CommBroker) {
         super(sliderPanel, commBroker);
         this.me = this;
         this.slideLeft = 'notes4';
         this.slideRight = 'notes2';
     }
 
-    private myInputText:string= '';
+    private myInputText: string = '';
     private show = true;
     private wheelDirection = 'wheel me in';
 
-    @Output() myKeyUp:EventEmitter<any> = new EventEmitter<any>();
+    @Output() myKeyUp: EventEmitter<any> = new EventEmitter<any>();
 
     private items: any[] = [
-        { title: 'Item 1' },
-        { title: 'Item 2' },
-        { title: 'Item 3' }
+        {title: 'Item 1'},
+        {title: 'Item 2'},
+        {title: 'Item 3'}
     ];
 
 
-    toggleShowHide(){
+    toggleShowHide() {
         this.show = !this.show;
     }
 
-    mouseWheel(direction, event){
+    mouseWheel(direction, event) {
         this.wheelDirection = direction;
     }
 }
