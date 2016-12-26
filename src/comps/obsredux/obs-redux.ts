28777c025838d0031f8a055444f7328cf8f9925a
root <born2net@gmail.com>
2016-12-26 11:48:46 -0800

    ng-redux using multiple examples of deep selection to bind to specific  reducer / values

--- src/comps/obsredux/obs-redux.ts
+++ src/comps/obsredux/obs-redux.ts
@@ -14,6 +14,7 @@ import {setTimeout} from "timers";
     selector: 'obs-redux',
     template: `
       I am : {{me}}
+      <br/>
       <button (click)="startStream()" class="btn">Start stream</button>
       <h5>StringJSPipe pipe: {{selectedReportNameLong | StringJSPipe:stringJSPipeArgs}}</h5>
       <hr/>
