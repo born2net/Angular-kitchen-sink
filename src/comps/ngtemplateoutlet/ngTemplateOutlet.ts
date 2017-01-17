82c607220b76e0451a47e8d7608221b68c77489e
root <born2net@gmail.com>
2017-01-17 12:41:39 -0800

    added match height component example

--- src/comps/ngtemplateoutlet/ngTemplateOutlet.ts
+++ src/comps/ngtemplateoutlet/ngTemplateOutlet.ts
@@ -26,6 +26,10 @@ export class ngTemplateOutletExample {}
     <button (click)="display = !display" style="margin-bottom: 20px;">Toggle content</button>
     <br>
     <template *ngIf="display" [ngTemplateOutlet]="templateRef"></template>
+     <!-- In ng v4+ NgTemplateOutlet now compatible with * syntax.-->
+    <!-- <ng-container *ngTemplateOutlet="templateRefExp; -->
+              <!--context: contextExp">-->
+    <!-- </ng-container>-->
   `,
 })
 export class TempRefContentExample {
